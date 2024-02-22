657e19693b3aef7cdee94dc46bd6fc4457889cdf
Keshav-Aneja <keshav.aneja09@gmail.com>
2024-02-23 02:28:37 +0530

    fix: Some ui fixes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,16 +1,37 @@
-import Image from "next/image"
-export default function Hero(){
-return (
+import Image from "next/image";
+export default function Hero() {
+  return (
     <main className="w-full flex h-[90vh] relative flex-col ">
-        <section className="flex flex-col gap-[1.5rem] absolute top-[50%] translate-y-[-50%]">
-        <h1 className="font-bold text-6xl w-[35%]">Best Learning Education Platform in the world</h1>
-        <p className="w-[40%] text-xl">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium eum odio vero molestiae quo? Vitae tenetur explicabo facilis quasi. Eveniet omnis non exercitationem! Quos itaque error quas repellendus. Reprehenderit, sint.</p>
-        <div className="bg-[#3538CD] text-white px-[1rem] py-[0.5rem] rounded-[0.5rem] w-fit">Get Started</div>
-        </section>
-        <section className="flex flex-col absolute right-0 top-[50%] translate-y-[-50%] gap-[1rem]">
-        <Image src={'/heroOne.avif'} width={400} height={400} alt="Logo" className="w-[90%] h-[90%]"/>
-        <Image src={'/heroTwo.avif'} width={400} height={400} alt="Logo" className="w-[90%] h-[90%]"/>
-        </section>
+      <section className="flex flex-col gap-[1.5rem] absolute top-[50%] translate-y-[-50%]">
+        <h1 className="font-bold text-6xl w-[50%]">
+          Best Learning Education Platform in the world
+        </h1>
+        <p className="w-[50%] text-lg">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium
+          eum odio vero molestiae quo? Vitae tenetur explicabo facilis quasi.
+          Eveniet omnis non exercitationem! Quos itaque error quas repellendus.
+          Reprehenderit, sint.
+        </p>
+        <div className="bg-[#3538CD] text-white px-[1rem] py-[0.5rem] rounded-[0.5rem] w-fit">
+          Get Started
+        </div>
+      </section>
+      <section className="flex flex-col absolute right-0 top-[50%] translate-y-[-50%] gap-[1rem]">
+        <Image
+          src={"/frontCover.png"}
+          width={400}
+          height={400}
+          alt="Logo"
+          className="w-[100%] h-[100%]"
+        />
+        {/* <Image
+          src={"/heroTwo.avif"}
+          width={400}
+          height={400}
+          alt="Logo"
+          className="w-[90%] h-[90%]"
+        /> */}
+      </section>
     </main>
-)
+  );
 }
--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,12 +1,24 @@
-import Image from "next/image"
-export default function Services(){
-    return (
-        <section className="w-full flex bg-[#A4BCFD] py-[1rem] px-[1rem] rounded-[0.5rem]">
-            <Image src={'/heroOne.avif'} width={400} height={400} alt="Logo" className="w-[30%] h-[30%] rounded-[0.5rem]"/>
-            <div className="flex flex-col gap-[1rem] items-end">
-                <h1 className="text-5xl font-bold">Our Services</h1>
-                <p className="text-lg w-[50%]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit maxime odio sunt Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit maxime odio sunt, expedita quo earum quasi nihil autem at perspiciatis nam porro aspernatur molestiae fugiat ut nulla officiis, aut quis.</p>
-            </div>
-        </section>
-    )
+import Image from "next/image";
+export default function Services() {
+  return (
+    <section className="w-full flex bg-[#e0e0e0] p-2 px-4 rounded-[0.5rem]">
+      <div className="flex flex-col gap-[1rem] items-start justify-center">
+        <h1 className="text-5xl font-bold">Our Services</h1>
+        <p className="text-lg w-[80%]">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+          maxime odio sunt Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Impedit maxime odio sunt, expedita quo earum quasi nihil autem
+          at perspiciatis nam porro aspernatur molestiae fugiat ut nulla
+          officiis, aut quis.
+        </p>
+      </div>
+      <Image
+        src={"/Cover-login.png"}
+        width={400}
+        height={400}
+        alt="Logo"
+        className="w-[30%] h-[30%] rounded-[0.5rem]"
+      />
+    </section>
+  );
 }
