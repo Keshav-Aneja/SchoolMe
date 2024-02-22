4b47db56826c4b4762f30a99908f1ce881cbac54
manaslaud <manaslaud@hotmail.com>
2024-02-22 20:49:57 +0530

    feat:jwtExpiry

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,20 @@
+"use client"
 import Image from "next/image"
+import { useRouter } from "next/navigation"
+import { checkExpiry } from "@/helpers/jwtExpiryChecker"
+import { useEffect,useState } from "react"
 export default function Navbar(){
+    const router = useRouter()
+    const [loggedIn,setLoggedIn]=useState<boolean>(false)
+    useEffect(()=>{
+        if(checkExpiry()){
+            setLoggedIn(false)
+            return
+        }
+        else{
+            setLoggedIn(true)
+        }
+    },[])
     return (
         <nav className="flex w-full py-[1rem] justify-between">
         <Image src={'/demoLogo.png'} width={145} height={145} alt="Logo"/>
@@ -7,8 +22,12 @@ export default function Navbar(){
                 <div className="cursor-pointer">Home</div>
                 <div className="cursor-pointer">About Us</div>
                 <div className="cursor-pointer">Features</div>
-                <div className="cursor-pointer">Login</div>
-                <div className="bg-[#A4BCFD] text-white px-[1rem] py-[0.5rem] rounded-[0.5rem] cursor-pointer">Register</div>
+                <div className="cursor-pointer" onClick={()=>{
+                    router.push('/login')
+                }}>{!loggedIn?'Login':'Logged In'}</div>
+                <div className="bg-[#A4BCFD] text-white px-[1rem] py-[0.5rem] rounded-[0.5rem] cursor-pointer" onClick={()=>{
+                    router.push('/signup')
+                }}>Register</div>
             </div>
         </nav>
     )
